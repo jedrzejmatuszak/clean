7a5e630a8beac8b04e63452eed752924c5d9cf3b
jedrzejmatuszak <jedrzej.matuszak@gmail.com>
2019-07-11 11:17:45 +0200

    api 0.1

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('load-flatmate/', load_flatmate, name='load-flatmate'),
     path('flat/', FlatList.as_view(), name='flat-list'),
     path('flat/<int:pk>/', FlatDetail.as_view(), name='flat-detail'),
+    path('flatmate/', FlatmateList.as_view(), name='flatmate-list'),
+    path('flatmate/<int:pk>/', FlatmateDetail.as_view(), name='flatmate-detail'),
+    path('room/', RoomList.as_view(), name='room-list'),
+    path('room/<int:pk>/', RoomDetail.as_view(), name='room-detail'),
+    path('cleanup/', CleanUpList.as_view(), name='cleanup-list'),
+    path('cleanup/<int:pk>/', CleanUpDetail.as_view(), name='cleanup-detail'),
+    path('record/', RecordList.as_view(), name='record-list'),
+    path('record/<int:pk>/', RecordDetail.as_view(), name='record-detail'),
 ]
--- clean_management/settings.py
+++ clean_management/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'api'
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
